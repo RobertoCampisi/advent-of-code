f93f37d614bbd635dd6d44f68a084c40536c55d6
Flizz95 <36100755+Flizz95@users.noreply.github.com>
2025-03-31 12:59:03 +0200

    Update day18p1.py

--- 2024/day18p1.py
+++ 2024/day18p1.py
@@ -1,117 +1,69 @@
-
-class Node():
-    """A node class for A* Pathfinding"""
-
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
+class Node:
+    def __init__(self, pos, par=None):
+        self.position = pos
+        self.parent = par
         self.g = 0
         self.h = 0
-        self.f = 0
-
+        self.f = self.g + self.h
+    
+    def __lt__(self, other):
+        return self.f < other.f
+    
     def __eq__(self, other):
         return self.position == other.position
-        
+    
     def __str__(self):
-        return f'({position[0]}, {position[1]})'
+        return f'{self.position}, g={self.g}, h={self.h}, f={self.f}'
 
+def get_neighbors(grid, pos):
+    return list(filter(lambda x: x in grid and grid[x] != 1, [pos+1,pos-1,pos+1j,pos-1j]))
 
-def astar(maze, start, end):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+def get_dist(p1, p2):
+    return abs(p1.real - p2.real) + abs(p1.imag - p2.imag)
 
-    # Create start and end node
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
-
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
-
-    # Add the start node
-    open_list.append(start_node)
-
-    # Loop until you find the end
-    while len(open_list) > 0:
-        # Get the current node
-        current_node = open_list[0]
-        current_index = 0
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
-
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
-        closed_list.append(current_node)
-
-        # Found the goal
-        if current_node == end_node:
+def astar(grid, start, goal):
+    open_set = [Node(start)]
+    closed_set = set()
+    while open_set:
+        current_node = open_set.pop(0)
+        closed_set.add(current_node.position)
+        
+        if current_node.position == goal:
             path = []
-            current = current_node
+            current = current_node.parent
             while current is not None:
                 path.append(current.position)
                 current = current.parent
-            return path[::-1] # Return reversed path
-
-        # Generate children
-        children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
-
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-            
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
-                continue
-
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != 0:
-                continue
-
-            # Create new node
-            new_node = Node(current_node, node_position)
-
-            # Append
-            children.append(new_node)
-
-        # Loop through children
-        for child in children:
-
-            # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
-
-            # Create the f, g, and h values
-            child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
-            child.f = child.g + child.h
-
-            # Child is already in the open list
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
-
-            # Add the child to the open list
-            open_list.append(child)
-
-memory = [[0]*7 for _ in range(7)]
-print(len(memory), len(memory[0]))
-with open("example.txt",'r') as f:
-    bytes = f.read().split("\n")
-    for byte in bytes[:12]:
-        x,y = map(int,byte.split(","))
-        #print(x,y)
-        memory[x][y] = 1
+            return list(reversed(path))
         
-start = (0, 0)
-end = (70, 70)
+        for n in get_neighbors(grid, current_node.position):
+            if n in closed_set:
+                continue
+            new_g = current_node.g + 1
+            neighbor_node = Node(n, current_node)
+            if neighbor_node in open_set:
+                if new_g >= neighbor_node.g:
+                    continue
+            neighbor_node.g = new_g
+            neighbor_node.h = get_dist(n,goal)
+            neighbor_node.f = neighbor_node.g + neighbor_node.h
+            open_set.append(neighbor_node)
+        open_set.sort()
+    return None
 
-for row in memory:
-    print(''.join(map(lambda x: '.' if x == 0 else '#', row)))
-    
+memory = {}
+for i in range(71):
+    for j in range(71):
+        memory[i+j*1j] = 0
+len(memory)
+with open("input.txt",'r') as f:
+    bytes = f.read().split("\n")
+    for byte in bytes[:1024]:
+        x,y = map(int,byte.split(","))
+        memory[x+y*1j] = 1
+        
+start = 0 + 0j
+end = 70 + 70j
+
 path = astar(memory, start, end)
-    
+print(len(path))
