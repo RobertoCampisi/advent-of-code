ba76f72c4e8088544c908c1c69a4dd24bc447289
Flizz95 <robertocampisi1995@gmail.com>
2025-04-25 13:33:32 +0200

    make day1.py more readable

--- 2024/day1.py
+++ 2024/day1.py
@@ -10,16 +10,18 @@ data = parse_input()
 def part_one(data=data):
     left, right = [], []
     for line in data:
-        left.append(int(line.split("   ")[0]))
-        right.append(int(line.split("   ")[1]))
+        l, r = list(map(int, line.split("   ")))
+        left.append(l)
+        right.append(r)
     print(sum([abs(a - b) for (a,b) in zip(sorted(left),sorted(right))]))
 
 
 def part_two(data=data):
     left, right = [], {}
     for line in data:
-        left.append(int(line.split("   ")[0]))
-        right[int(line.split("   ")[1])] = right.get(int(line.split("   ")[1]),0) + 1
+        l, r = list(map(int, line.split("   ")))
+        left.append(l)
+        right[r] = right.get(r,0) + 1
     print(sum([a * right.get(a,0) for a in left]))
 
 if __name__ == '__main__':
