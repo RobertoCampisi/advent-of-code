6e97a9740c686bcedfe4a689f37be53f6d6fd3be
Flizz95 <robertocampisi1995@gmail.com>
2025-04-25 09:39:50 +0200

    added template

---
+++ template.py
@@ -0,0 +1,13 @@
+#import util functions
+
+def parse_input()
+    with open('{0}/input/day{1}.txt','r') as input:
+        return ...
+
+data = parse_input()
+
+def part_one(data=data):
+    ...
+
+def part_two(data=data):
+    ...
