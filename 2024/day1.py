f0bd13d64c2b7500f7b623aebfe69cf1deab6ebd
Roberto Campisi <robertocampisi1995@gmail.com>
2025-04-29 23:14:11 +0200

    benchmark function changed to milliseconds

--- 2024/day1.py
+++ 2024/day1.py
@@ -26,5 +26,20 @@ def part_two():
         right[r] = right.get(r,0) + 1
     print(sum([a * right.get(a,0) for a in left]))
 
+def benchmark(func, n):
+    from time import time_ns
+    start = time_ns() // 1_000_000
+    sys.stdout = None  # bit hacky but it works
+    for i in range(0, int(n)):
+        globals()[func]()
+    sys.stdout = sys.__stdout__ #restore
+    end = time_ns() // 1_000_000
+    print((end - start) / int(n), end='')
+
 if __name__ == '__main__':
-    globals()[sys.argv[1]]()
+    if len(sys.argv) == 2:
+        globals()[sys.argv[1]]()
+    elif len(sys.argv) > 2:
+        globals()[sys.argv[1]](*sys.argv[2:])
+    else:
+        raise RuntimeError
