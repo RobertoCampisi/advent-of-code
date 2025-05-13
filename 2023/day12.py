c0b72c7f9720a6c3771caed2b3bf567df0d27d31
Roberto Campisi <robertocampisi1995@gmail.com>
2025-05-13 16:13:33 +0200

    optimized 2023 day 12 by looking at a example using permuations

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from functools import cache
+from collections import defaultdict
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 #import my_util functions
 
@@ -13,59 +14,35 @@ def parse_input():
             data.append((arrng_str, tuple([int(c) for c in code.split(',')])))
     return data
 
-@cache
-def valid(arrangement, code):
-    if not code:
-        return '#' not in arrangement
-    last = code[-1]
-    if len(code) == 1 & len(arrangement) > last:
-        return False
-    k = 0
-    c = 0
-    for sym in arrangement[::-1]:
-        k-=1
-        if sym == '#':
-            c += 1
-        elif sym == '.' and c > 0:
-            if c != last:
-                return False
-            else:
-                break
-        elif sym != '.':
-            return False
-    if c == 0:
-        return False
-    if len(code) == 1 and c != last:
-        return False
-    return valid(arrangement[:k],code[:-1])
-
-
-def total_arrangements(ar_string,code):
-    print(ar_string, code)
-    res = 0
-    if '?' not in ar_string:
-        if valid(ar_string, code):
-            print('Valid')
-            res += 1
-    else:
-        for i in range(len(ar_string)):
-            if ar_string[i] == '?' and i < len(ar_string) - 1:
-                res += total_arrangements(ar_string[:i] + '.' + ar_string[i+1:], code)
-                res += total_arrangements(ar_string[:i] + '#' + ar_string[i+1:], code)
-                break
-            elif ar_string[i] == '?':
-                res += total_arrangements(ar_string[:i] + '.', code)
-                res += total_arrangements(ar_string[:i] + '#', code)
-                break
-    return res
+def total_arrangements(_string,code):
+    def is_valid(_id, _amount): #helper function
+        return _id == len(code) or _id == len(code) - 1 and _amount == code[_id]
+    perms = defaultdict(int)
+    perms[(0,0)] = 1 #(_id, _amount)
+    for sym in _string:
+        next_it = []
+        for key, perm_count in perms.items():
+            _id, _amount = key
+            if sym != '#':
+                if _amount == 0:
+                    next_it.append((_id,_amount,perm_count))
+                elif _amount == code[_id]:
+                    next_it.append((_id+1,0,perm_count))
+            if sym != '.':
+                if _id < len(code) and _amount < code[_id]:
+                    next_it.append((_id,_amount + 1, perm_count))
+        perms.clear()
+        for _id, _amount, perm_count in next_it:
+            perms[(_id,_amount)] += perm_count
+    return sum(a for k,a in perms.items() if is_valid(*k))
 
 def part_one():
     data = parse_input()
     print(sum([total_arrangements(ar_string,code) for ar_string, code in data]))
 
 def part_two():
-    data = parse_input()[:1]
-    print(sum([total_arrangements(ar_string*5, tuple(list(code)*5)) for ar_string, code in data]))
+    data = parse_input()
+    print(sum([total_arrangements('?'.join([ar_string]*5), tuple(list(code)*5)) for ar_string, code in data]))
 
 #simple benchmark function.
 def benchmark(func, n):
