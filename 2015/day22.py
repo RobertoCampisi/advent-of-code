cf35892ab3df749f385c3ac24067784c1e78a35d
Flizz95 <robertocampisi1995@gmail.com>
2025-06-05 11:33:39 +0200

    solved 2015 day 22: fixed mistake and BFS to DFS

--- 2015/day22.py
+++ 2015/day22.py
@@ -25,7 +25,7 @@ def part_one():
     least_mana_spend = math.inf
     queue = [([0] * len(spells), [hp, mana, 0], boss, True)]
     while queue:
-        active_spells, player, boss, player_turn = queue.pop(0)
+        active_spells, player, boss, player_turn = queue.pop(-1)
         armor = 0
         for i, spell_duration in enumerate(active_spells):
             if spell_duration > 0:
@@ -34,33 +34,64 @@ def part_one():
                 boss[0] -= spells[i][1]
                 player[1] += spells[i][5]
                 active_spells[i] -= 1
-
         if boss[0] <= 0:
             least_mana_spend = min(least_mana_spend, player[2])
         elif player_turn:
             could_act = False
             for i, spell in enumerate(spells):
-                if not active_spells[i] and player[1] >= spells[i][0]:
+                if active_spells[i] == 0 and player[1] >= spells[i][0]:
                     could_act = True
                     new_player = [player[0], player[1] - spells[i][0], player[2] + spells[i][0]]
                     new_active_spells = active_spells.copy()
                     new_active_spells[i] += spells[i][3]
                     if new_player[2] < least_mana_spend:
-                        print(new_active_spells, new_player, boss, False)
                         queue.append((new_active_spells, new_player , boss.copy(), False))
             if not could_act and player[2] < least_mana_spend:
                 queue.append((active_spells.copy(), player.copy(), boss.copy(), False))
         else:
-            player[0] = max(0,boss[1] - armor)
+            player[0] -= max(0,boss[1] - armor)
             if player[0] > 0 and player[2] < least_mana_spend:
-                queue.append((active_spells, player, boss, True))
+                queue.append((active_spells.copy(), player.copy(), boss.copy(), True))
     print(least_mana_spend)
 
-
-
-
 def part_two():
-    ...
+    boss = parse_input()
+    hp = 50
+    mana = 500
+    least_mana_spend = math.inf
+    queue = [([0] * len(spells), [hp, mana, 0], boss, True)]
+    while queue:
+        active_spells, player, boss, player_turn = queue.pop(-1)
+        if player[0] > 0:
+            if player_turn:
+                player[0] -= 1
+            armor = 0
+            for i, spell_duration in enumerate(active_spells):
+                if spell_duration > 0:
+                    player[0] += spells[i][2]
+                    armor += spells[i][4]
+                    boss[0] -= spells[i][1]
+                    player[1] += spells[i][5]
+                    active_spells[i] -= 1
+            if boss[0] <= 0:
+                least_mana_spend = min(least_mana_spend, player[2])
+            elif player_turn:
+                could_act = False
+                for i, spell in enumerate(spells):
+                    if active_spells[i] == 0 and player[1] >= spells[i][0]:
+                        could_act = True
+                        new_player = [player[0], player[1] - spells[i][0], player[2] + spells[i][0]]
+                        new_active_spells = active_spells.copy()
+                        new_active_spells[i] += spells[i][3]
+                        if new_player[2] < least_mana_spend:
+                            queue.append((new_active_spells, new_player , boss.copy(), False))
+                if not could_act and player[2] < least_mana_spend:
+                    queue.append((active_spells.copy(), player.copy(), boss.copy(), False))
+            else:
+                player[0] -= max(0,boss[1] - armor)
+                if player[0] > 0 and player[2] < least_mana_spend:
+                    queue.append((active_spells.copy(), player.copy(), boss.copy(), True))
+    print(least_mana_spend)
 
 #simple benchmark function.
 def benchmark(func, n):
